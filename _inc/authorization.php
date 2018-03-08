6a2094df60e54a8ec0e8477c54c94561ac6c319f
Jakub Vyskoc <jakub.vyskoc@gmail.com>
2018-03-08 08:00:42 +0100

    nejake male zmeny

--- _inc/authorization.php
+++ _inc/authorization.php
@@ -1,9 +1,9 @@
 <?php
 
 // Includes
-require_once 'vendor/phpauth/phpauth/languages/en_GB.php' ;
-require_once 'vendor/phpauth/phpauth/Config.php' ;
-require_once 'vendor/phpauth/phpauth/Auth.php' ;
+require_once 'vendor_edited/phpauth/phpauth/languages/en_GB.php' ;
+require_once 'vendor_edited/phpauth/phpauth/Config.php' ;
+require_once 'vendor_edited/phpauth/phpauth/Auth.php' ;
 
 $dbh = new PDO("mysql:host=localhost;dbname=jobi", "root", "root");
 
